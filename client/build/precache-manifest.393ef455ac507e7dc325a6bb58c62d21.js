431b604d010dfe5523e878e641b0a638b1e76167
leonardoj6 <l.jun1717@gmail.com>
2020-09-07 18:56:50 -0600

    add client build

---
+++ client/build/precache-manifest.393ef455ac507e7dc325a6bb58c62d21.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5959896da32affacba6efe84cf5a2629",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5aa05524d232acfc5f0",
+    "url": "/static/css/main.b0efa8ef.chunk.css"
+  },
+  {
+    "revision": "e49e568e4351a3157b24",
+    "url": "/static/js/2.73b18dc9.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.73b18dc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5aa05524d232acfc5f0",
+    "url": "/static/js/main.e7b8c2fb.chunk.js"
+  },
+  {
+    "revision": "2189ad404f108a6437fb",
+    "url": "/static/js/runtime-main.f141b71d.js"
+  },
+  {
+    "revision": "6a235456462ff339ef3bd148d73b144e",
+    "url": "/static/media/Todoie_logo.6a235456.svg"
+  },
+  {
+    "revision": "cb7b7a58ebb9271c1132e116a6b43766",
+    "url": "/static/media/landingpage-background.cb7b7a58.jpg"
+  },
+  {
+    "revision": "183dc3a68eeb7183715704b612a0fdea",
+    "url": "/static/media/logo.183dc3a6.svg"
+  },
+  {
+    "revision": "c135dbbd2d2a74ce25ef966dc3e936d4",
+    "url": "/static/media/todolist_item--completed.c135dbbd.svg"
+  },
+  {
+    "revision": "74254da3457f073745d11ab5b5b60828",
+    "url": "/static/media/todolist_item--incomplete.74254da3.svg"
+  }
+]);
