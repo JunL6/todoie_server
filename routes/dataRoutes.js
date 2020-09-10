46674b9825e92420d0de812472c80fe7b8fcadce
leonardoj6 <l.jun1717@gmail.com>
2020-09-10 14:33:20 -0600

    refactor server side code: seperate DB logic from route handling logic

--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -1,56 +1,50 @@
 const mongoose = require("mongoose");
 const log = require("../utils/logging");
+const { getUser, insertTodo, addGroup } = require("../services/user.service");
 
 const User = mongoose.model("User");
 
 module.exports = (app) => {
   app.get("/api/getUserData", (req, res) => {
-    log(`[/api/getUserData] ${req.user.userID}`);
+    const { userID } = req.user;
+    log(`[/api/getUserData] userID: ${userID}`);
     if (!req.user) {
       return res.status(401).send("Not authorized");
     } else {
-      User.findOne({ userID: req.user.userID }).exec((err, userDoc) => {
-        if (err) {
-          console.error(err);
-          res.send(`error has occured.`);
-        } else {
+      getUser(req.user.userID)
+        .then((userDoc) => {
           res.json(userDoc);
-        }
-      });
+        })
+        .catch((err) => {
+          console.error(err);
+          res.send(`[error]: can't get user data`);
+        });
     }
   });
 
   app.post("/api/insertTodo", (req, res) => {
-    log(`[/api/insertTodo] ${req.user.userID}`);
+    const { userID } = req.user;
+    log(`[/api/insertTodo] userID: ${userID}`);
     if (!req.user) {
       return res.status(401).send("Not authorized");
     } else {
       const { groupId, todoContent, timeCreated, ...rest } = req.body;
-      User.findOne({ userID: req.user.userID }).exec((err, userDoc) => {
-        if (err) {
+
+      insertTodo(userID, groupId, todoContent, timeCreated)
+        .then(() => {
+          res.send("todo inserted");
+        })
+        .catch((err) => {
           console.error(err);
-          res.send(`error has occured.`);
-        } else {
-          userDoc.todos.push({
-            groupId,
-            todoContent,
-            isCompleted: false,
-            timeCreated,
-          });
-          userDoc.save((err) => {
-            if (err) {
-              console.error(err);
-            } else {
-              res.send("todo inserted");
-            }
-          });
-        }
-      });
+          res.send(`error has occurred: ${err}`);
+        });
     }
   });
 
+  /* todo: refactor this code to user.service.js */
   app.post("/api/updateTodo", (req, res) => {
-    log(`[update todo] ${req.user.userID}`);
+    const { userID } = req.user;
+    log(`[update todo] userID: ${userID}`);
     if (!req.user) {
       return res.status(401).send("Not authorized");
     } else {
@@ -83,7 +77,6 @@ module.exports = (app) => {
           });
         }
 
-        // console.log(userDoc.todos);
         userDoc.save((err) => {
           if (err) console.error(err);
           else res.send(`todo item updated!`);
@@ -93,31 +86,40 @@ module.exports = (app) => {
   });
 
   app.post("/api/addGroup", (req, res) => {
-    log(`[add group] ${req.user.userID}`);
+    const { userID } = req.user;
+    log(`[add group] ${userID}`);
     if (!req.user) {
       return res.status(401).send("Not authorized");
     }
 
     const { groupName, timeCreated } = req.body;
-    User.findOne({ userID: req.user.userID }).exec((err, userDoc) => {
-      if (err) {
-        console.error(err);
-        return res.send(`error has occurred during DB executions`);
-      } else {
-        userDoc.groups.push({
-          groupName,
-          timeCreated,
-        });
 
-        userDoc.save((err, newUserDoc) => {
-          if (err) {
-            console.error(err);
-            return res.send(`error occurred during DB saving`);
-          } else {
-            res.send(newUserDoc.groups[newUserDoc.groups.length - 1]);
-          }
-        });
-      }
-    });
+    addGroup(userID, groupName, timeCreated)
+      .then((newGroup) => res.send(newGroup))
+      .catch((err) => {
+        console.error(err);
+        res.send(`error has occurred: ${err}`);
+      });
+
+    // User.findOne({ userID: req.user.userID }).exec((err, userDoc) => {
+    //   if (err) {
+    //     console.error(err);
+    //     return res.send(`error has occurred during DB executions`);
+    //   } else {
+    //     userDoc.groups.push({
+    //       groupName,
+    //       timeCreated,
+    //     });
+
+    //     userDoc.save((err, newUserDoc) => {
+    //       if (err) {
+    //         console.error(err);
+    //         return res.send(`error occurred during DB saving`);
+    //       } else {
+    //         res.send(newUserDoc.groups[newUserDoc.groups.length - 1]);
+    //       }
+    //     });
+    //   }
+    // });
   });
 };
--- services/user.service.js
+++ services/user.service.js
@@ -3,5 +3,41 @@ const mongoose = require("mongoose");
 const User = mongoose.model("User");
 
 async function getUser(userID) {
-  return await User.findOne({ UserID });
+  return await User.findOne({ userID });
 }
+
+/* 为何这里要用async await: 因为要return一个promise */
+async function insertTodo(userID, groupId, todoContent, timeCreated) {
+  return await User.findOne({ userID })
+    .then(async (userDoc) => {
+      userDoc.todos.push({
+        groupId,
+        todoContent,
+        isCompleted: false,
+        timeCreated,
+      });
+      return await userDoc.save();
+    })
+    .catch((err) => {
+      console.error(err);
+      throw Error(`can't find user document`);
+    });
+}
+
+async function addGroup(userID, groupName, timeCreated) {
+  return await User.findOne({ userID })
+    .then(async (userDoc) => {
+      userDoc.groups.push({
+        groupName,
+        timeCreated,
+      });
+      let newUserDoc = await userDoc.save();
+      return newUserDoc.groups[newUserDoc.groups.length - 1];
+    })
+    .catch((err) => {
+      console.error(err);
+      throw Error(`can't find user document`);
+    });
+}
+
+module.exports = { getUser, insertTodo, addGroup };
